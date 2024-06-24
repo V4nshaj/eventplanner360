229bd0be1b865e6e192961d725d48180b2924d04
V4nshaj <vanshajbarnwal21@gmail.com>
2024-06-24 22:12:51 +0530

    added login & signin

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link"
 import Image from 'next/image';
-import { SignedOut } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignIn, UserButton } from '@clerk/nextjs';
 import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
     return (
@@ -10,7 +12,18 @@ const Header = () => {
                 <Link href='/' className="w-36">
                     <Image src='/assets/images/logo.svg' width={128} height={38} alt="EventPlanner360 Logo" />
                 </Link>
+
+                <SignedIn>
+                    <nav className="md:flex-between hidden w-full max-w-xs">
+                        <NavItems />
+                    </nav>
+                </SignedIn>
+
                 <div className="flex w-32 justify-end gap-3">
+                    <SignedIn>
+                        <UserButton afterSignOutUrl="/" />
+                        <MobileNav />
+                    </SignedIn>
                     <SignedOut>
 
                         <Button asChild className="rounded-full" size="lg">
