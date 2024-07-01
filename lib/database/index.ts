ee76b0a9466d7efdd1fd383769a09b34dbc42e21
V4nshaj <vanshajbarnwal21@gmail.com>
2024-07-01 13:38:44 +0530

    database models created

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -4,14 +4,15 @@ const MONGODB_URI = process.env.MONGODB_URI;//Url/uri we want connect to
 
 let cached = (global as any).mongoose || {conn: null, promise: null};//if we dont have mongoose cached then set it to null
 //global as any above is used to access global variables in Node.js here imported mongoose is not a part of imported module but a global variable in Node.js
+//cached is used to cached connection to the database
 export const connectoDatabase = async () =>{
-    if(cached.conn) return cached.conn;//cached.conn check if funcn already holds Mongodb(mongoose) connection This ensures that the function doesn't attempt to connect to MongoDB multiple times if a connection is already established and cached.
+    if(cached.conn) return cached.conn;//cached.conn check if funcn already holds Mongodb(mongoose) connection i.e cache is already connected This ensures that the function doesn't attempt to connect to MongoDB multiple times if a connection is already established and cached. Here connection runs for the first time.
 
     if(!MONGODB_URI) throw new Error('MongoDB URI is missing');//if no mongoDB_URI is provided then throw an error
 
     //if we have cached conncetion from above
     //then return the cached connection else create a new mongoose connection
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {//here we are connect to already established mongoose connection or create a new one
         dbName: 'EventPlanner360',
         bufferCommands: false,//disable buffering on all models associated with this connection.
     });
---
+++ lib/database/models/user.models.ts
@@ -0,0 +1,16 @@
+import { Schema, model, models } from "mongoose";
+
+
+const UserSchema = new Schema({
+    clerkId: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    userName: {type: String, required: true, unique: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    photo: {type: String, required: true},
+})
+
+//Using the above schema to create a model named 'User'
+const User = models.User || model('User', UserSchema);//models.User is the existing model or creates a new one by using schema if it doesn't exist
+
+export default User;
